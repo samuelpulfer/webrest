05b20a27ed45015c33bc6acf24caf1d889a15ff8
wus <swunderlin@gmail.com>
2016-03-08 18:09:55 +0100

    added dict, xml, html and txt renderer

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,5 +2,6 @@
 
 cd lib
 tar xzf web.py-0.37.tar.gz
-unzip mimerender-master.zip
+unzip -o -q mimerender-master.zip
+tar xzf dicttoxml-1.6.6.tar.gz
 cd ..
--- httpd.py
+++ httpd.py
@@ -10,8 +10,13 @@ import sys, os
 #mimerender = mimerender.WebPyMimeRender()
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'lib', 'web.py-0.37'))
 sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'lib', 'mimerender-master', 'src'))
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'lib', 'dicttoxml-1.6.6'))
+
+import pprint
 import mimerender
 import web
+import dicttoxml
+from xml.dom.minidom import parseString
 
 mimerender = mimerender.WebPyMimeRender()
 
@@ -19,31 +24,63 @@ urls = (
 	'/(.*)', 'greet'
 )
 
-class MyApplication(web.application):
+class wsgiapp(web.application):
 	def run(self, port=8080, ip='127.0.0.1', *middleware):
 		func = self.wsgifunc(*middleware)
 		return web.httpserver.runsimple(func, (ip, port))
 
+class renderer(object):
+	
+	@staticmethod
+	def dict2xml(d):
+		return parseString(dicttoxml.dicttoxml(d, \
+		                   custom_root="root")).toprettyxml()
+
+	@staticmethod
+	def dict2json(d):
+		return json.dumps(d)
+		
+	@staticmethod
+	def dict2txt(d):
+		pp = pprint.PrettyPrinter(indent=4, width=1)
+		return pp.pformat(d)
+
+	@staticmethod
+	def dict2html(d):
+		# FIXME: handle lists and tuples
+		buffer = '<table border="1" style="border-collapse: collapse;"><tbody>'
+		for e in d:
+			buffer += "<tr><td>" + str(e) + "</td><td>"
+			if type(d[e]) is dict: # or type(d[e]) is list:
+				buffer += renderer.dict2html(d[e])
+			else:
+				buffer += str(d[e])
+			buffer += "</td></tr>"
+		buffer += '</tbody></table>'
+	
+		return buffer
 
 class greet:
-	render_xml  = lambda message: '<message>%s</message>'%message
-	render_json = lambda **args: json.dumps(args)
-	render_html = lambda message: '<html><body>%s</body></html>'%message
-	render_txt  = lambda message: message
+	render_xml  = lambda **args: renderer.dict2xml(args)
+	render_json = lambda **args: renderer.dict2json(args)
+	render_html = lambda **args: '<!DOCTYPE html>\n<html>\n<body>\n' + \
+	                             renderer.dict2html(args) + "\n</body>\n</html>"
+	render_txt  = lambda **args: renderer.dict2txt(args)
 
 	@mimerender(
-		default = 'html',
-		html = render_html,
-		xml  = render_xml,
-		json = render_json,
+		default = 'txt',
+		#html = render_html,
+		#json = render_json,
+		#xml  = render_xml,
 		txt  = render_txt
 	)
 	
 	def GET(self, name):
 		if not name: 
 			name = 'world'
-		return {'message': 'Hello, ' + name + '!'}
+		return {'message': 'Hello, ' + name + '!', "sub": {"array": [1,2,3], "list": {"a": 1, "b": 2}}}
 
 if __name__ == "__main__":
-	app = MyApplication(urls, globals())
+	web.config.debug = True
+	app = wsgiapp(urls, globals())
 	app.run(port=8082)
